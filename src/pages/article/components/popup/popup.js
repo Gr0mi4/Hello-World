317dced9c9cebb252a6068858d78c05683391e49
Ivan Gromov <50546979+Gr0mi4@users.noreply.github.com>
2019-11-18 15:36:25 +0300

    Ok - mistakes fixed, method articlePopup.init - added

--- src/pages/article/components/popup/popup.js
+++ src/pages/article/components/popup/popup.js
@@ -1,18 +1,20 @@
-const popup = function () {
-  const question = document.querySelector('.question');
-  const newsletter = document.querySelector('.newsletter');
-  const sorryMessage = document.querySelector('.sorry-message');
-  const thankMessage = document.querySelector('.thank-message');
-  const maxHeight = Math.max(
+var articlePopup = (function() {
+  'use strict';
+  const popup = document.querySelector(".popup");
+  const question = popup.querySelector('.question');
+  const newsletter = popup.querySelector('.newsletter');
+  const sorryMessage = popup.querySelector('.sorry-message');
+  const thankMessage = popup.querySelector('.thank-message');
+  const pageHeight = Math.max(
     document.body.scrollHeight, document.documentElement.scrollHeight,
     document.body.offsetHeight, document.documentElement.offsetHeight,
     document.body.clientHeight, document.documentElement.clientHeight
   );
-  const height = maxHeight - document.documentElement.clientHeight;
-  const yesButton = document.querySelector('.yes-button');
-  const noButton = document.querySelector('.no-button');
-  const cross = document.querySelector('.cross');
-  const submit = document.querySelector('.submit-button');
+  const scrollHeight = pageHeight - document.documentElement.clientHeight;
+  const yesButton = popup.querySelector('.yes-button');
+  const noButton = popup.querySelector('.no-button');
+  const cross = popup.querySelector('.cross');
+  const submit = popup.querySelector('.submit-button');
 
   const invisibility = function (classname) {
     classname.classList.remove('visible');
@@ -22,35 +24,58 @@ const popup = function () {
     classname.classList.add('visible');
   };
 
-  window.addEventListener('scroll', function () {
-    if (pageYOffset > height && localStorage['quantity'] === undefined) {
-      visibility(question);
-      localStorage['quantity'] = '1';
+  function addScrollListener() {
+    if (localStorage['popupClosed'] === undefined) {
+      window.addEventListener('scroll', function () {
+        if (pageYOffset > scrollHeight) {
+          visibility(popup);
+          localStorage['popupClosed'] = true;
+        }
+      });
     }
-  });
+  }
+  addScrollListener();
 
   yesButton.addEventListener('click', function () {
-    invisibility(question);
-    visibility(newsletter);
+    function showNewsLetter() {
+      invisibility(question);
+      visibility(newsletter);
+    }
+    showNewsLetter();
   });
 
   noButton.addEventListener('click', function () {
-    invisibility(question);
-    visibility(sorryMessage);
-    setTimeout(invisibility, 4000, sorryMessage);
+    function showSorryMessage () {
+      invisibility(question);
+      visibility(sorryMessage);
+      setTimeout(invisibility, 4000, sorryMessage);
+      setTimeout(invisibility, 4000, popup);
+    }
+    showSorryMessage();
   });
 
   cross.addEventListener('click', function () {
-    invisibility(newsletter);
+    function hideNewsLetter() {
+      invisibility(newsletter);
+      invisibility(popup);
+    }
+    hideNewsLetter();
   });
 
   submit.addEventListener('click', function (evt) {
     evt.preventDefault();
-    invisibility(newsletter);
-    visibility(thankMessage);
-    setTimeout(invisibility, 3000, thankMessage);
+    function showThankMessage() {
+      invisibility(newsletter);
+      visibility(thankMessage);
+      setTimeout(invisibility, 3000, thankMessage);
+      setTimeout(invisibility, 3000, popup);
+    }
+    showThankMessage();
   });
-
-  return localStorage['quantity'];
-};
-popup();
+  return  {
+    init: function () {
+      visibility(popup);
+      visibility(question);
+    }
+  }
+}());
