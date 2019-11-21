46195c1145a2ea0860560fbe8e4a94c467f143b5
Ivan Gromov <50546979+Gr0mi4@users.noreply.github.com>
2019-11-21 18:54:27 +0300

    another refactoring

--- src/pages/article/components/popup/popup.js
+++ src/pages/article/components/popup/popup.js
@@ -1,89 +1,113 @@
-var articlePopup = (function() {
+window.articlePopup = (function articlePopup() {
   'use strict';
-  const popup = document.querySelector(".popup");
-  const question = popup.querySelector('.question');
-  const newsletter = popup.querySelector('.newsletter');
-  const sorryMessage = popup.querySelector('.sorry-message');
-  const thankMessage = popup.querySelector('.thank-message');
-  const pageHeight = Math.max(
-    document.body.scrollHeight, document.documentElement.scrollHeight,
-    document.body.offsetHeight, document.documentElement.offsetHeight,
-    document.body.clientHeight, document.documentElement.clientHeight
-  );
-  const scrollHeight = pageHeight - document.documentElement.clientHeight;
-  const yesButton = popup.querySelector('.yes-button');
-  const noButton = popup.querySelector('.no-button');
-  const cross = popup.querySelector('.cross');
-  const submit = popup.querySelector('.submit-button');
+  console.log('Запуск');
+  let popup;
+  let question;
+  let newsletter;
+  let sorryMessage;
+  let thankMessage;
+  let pageHeight;
+  let scrollHeight;
+  let yesButton;
+  let noButton;
+  let cross;
+  let submit;
 
-  function invisibility (classname) {
+  function removeVisibleClass (classname) {
     classname.classList.remove('visible');
   }
 
-  function visibility(classname) {
-    classname.classList.add('visible');
+  function addVisibleClass (element) {
+    element.classList.add('visible');
   }
 
   function showPopup() {
-    if (pageYOffset > scrollHeight && localStorage['popupClosed'] === undefined) {
-    visibility(popup);
-    visibility(question);
-    localStorage['popupClosed'] = true;
+    if (pageYOffset > scrollHeight && ! localStorage.popupClosed) {
+      addVisibleClass(popup);
+      addVisibleClass(question);
+      localStorage.popupClosed = true;
     }
   }
 
   function showNewsletter() {
-    invisibility(question);
-    visibility(newsletter);
+    removeVisibleClass(question);
+    addVisibleClass(newsletter);
   }
 
   function showSorryMessage() {
-    invisibility(question);
-    visibility(sorryMessage);
-    setTimeout(invisibility, 4000, sorryMessage);
-    setTimeout(invisibility, 4000, popup);
+    removeVisibleClass(question);
+    addVisibleClass(sorryMessage);
+    setTimeout(removeVisibleClass, 4000, sorryMessage);
+    setTimeout(removeVisibleClass, 4000, popup);
   }
 
   function hideNewsletter() {
-    invisibility(newsletter);
-    invisibility(popup);
+    removeVisibleClass(newsletter);
+    removeVisibleClass(popup);
   }
 
   function showThankMessage() {
-    invisibility(newsletter);
-    visibility(thankMessage);
-    setTimeout(invisibility, 3000, thankMessage);
-    setTimeout(invisibility, 3000, popup);
+    removeVisibleClass(newsletter);
+    addVisibleClass(thankMessage);
+    setTimeout(removeVisibleClass, 3000, thankMessage);
+    setTimeout(removeVisibleClass, 3000, popup);
   }
 
-  function addListeners() {
-    if (localStorage['popupClosed'] === undefined) {
-      window.addEventListener('scroll', showPopup);
-      yesButton.addEventListener('click', showNewsletter);
-      noButton.addEventListener('click', showSorryMessage);
-      cross.addEventListener('click', hideNewsletter);
-      submit.addEventListener('click', function (evt) {
-        evt.preventDefault();
-        showThankMessage();
-      });
-    }
+  function submitButton(evt) {
+    evt.preventDefault();
+    showThankMessage();
   }
-  addListeners();
+
+  function addListeners () {
+    window.addEventListener('scroll', showPopup);
+    yesButton.addEventListener('click', showNewsletter);
+    noButton.addEventListener('click', showSorryMessage);
+    cross.addEventListener('click', hideNewsletter);
+    submit.addEventListener('click', submitButton);
+  }
+
+  function show() {
+    addVisibleClass(popup);
+    addVisibleClass(question);
+  }
+
+  function hide() {
+    removeVisibleClass(question);
+    removeVisibleClass(newsletter);
+    removeVisibleClass(thankMessage);
+    removeVisibleClass(sorryMessage);
+    removeVisibleClass(popup);
+  }
+
+  function init() {
+    popup = document.querySelector('.popup');
+    question = popup.querySelector('.question');
+    newsletter = popup.querySelector('.newsletter');
+    sorryMessage = popup.querySelector('.sorry-message');
+    thankMessage = popup.querySelector('.thank-message');
+    pageHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    );
+    scrollHeight = pageHeight - document.documentElement.clientHeight;
+    yesButton = popup.querySelector('.yes-button');
+    noButton = popup.querySelector('.no-button');
+    cross = popup.querySelector('.cross');
+    submit = popup.querySelector('.submit-button');
+
+    if (!localStorage.popupClosed) {
+      addListeners();
+    }
+    else {console.log('Не работает')}
+  }
+  init();
 
   return  {
-    init: function () {
-      addListener()
-    },
-    show: function () {
-      visibility(popup);
-      visibility(question);
-    },
-    hide: function () {
-      invisibility(question);
-      invisibility(newsletter);
-      invisibility(thankMessage);
-      invisibility(sorryMessage);
-      invisibility(popup);
-    }
-  };
-}());
+    init,
+    show,
+    hide
+  }
+  }
+  )();
+
