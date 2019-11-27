5066c2005ea7f44a45de54a99f7fa5a8bdbe8ef0
Ivan Gromov <50546979+Gr0mi4@users.noreply.github.com>
2019-11-27 19:11:09 +0300

    another refactoring

--- src/pages/article/components/popup/popup.js
+++ src/pages/article/components/popup/popup.js
@@ -1,113 +1,112 @@
 window.articlePopup = (function articlePopup() {
-  'use strict';
-  console.log('Запуск');
-  let popup;
-  let question;
-  let newsletter;
-  let sorryMessage;
-  let thankMessage;
-  let pageHeight;
-  let scrollHeight;
-  let yesButton;
-  let noButton;
-  let cross;
-  let submit;
+    'use strict';
+    let popupElement;
+    let question;
+    let newsletter;
+    let sorryMessage;
+    let thankMessage;
+    let pageHeight;
+    let scrollHeight;
+    let yesButton;
+    let noButton;
+    let cross;
+    let submit;
 
-  function removeVisibleClass (classname) {
-    classname.classList.remove('visible');
-  }
+    function addVisibleClass(element) {
+      element.classList.add('visible');
+    }
 
-  function addVisibleClass (element) {
-    element.classList.add('visible');
-  }
+    function removeVisibleClass(element) {
+      element.classList.remove('visible');
+    }
 
-  function showPopup() {
-    if (pageYOffset > scrollHeight && ! localStorage.popupClosed) {
-      addVisibleClass(popup);
+    function showPopup() {
+      pageHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+      );
+      scrollHeight = pageHeight - document.documentElement.clientHeight;
+
+      if (pageYOffset >= scrollHeight && !localStorage.popupClosed) {
+        addVisibleClass(popupElement);
+        addVisibleClass(question);
+        localStorage.popupClosed = true;
+        removeEventListener('scroll', showPopup);
+      }
+    }
+
+    function showNewsletter() {
+      removeVisibleClass(question);
+      addVisibleClass(newsletter);
+    }
+
+    function showSorryMessage() {
+      removeVisibleClass(question);
+      addVisibleClass(sorryMessage);
+      setTimeout(removeVisibleClass, 4000, sorryMessage);
+      setTimeout(removeVisibleClass, 4000, popupElement);
+    }
+
+    function hideNewsletter() {
+      removeVisibleClass(newsletter);
+      removeVisibleClass(popupElement);
+    }
+
+    function showThankMessage() {
+      removeVisibleClass(newsletter);
+      addVisibleClass(thankMessage);
+      setTimeout(removeVisibleClass, 3000, thankMessage);
+      setTimeout(removeVisibleClass, 3000, popupElement);
+    }
+
+    function onSubmitButtonClick(event) {
+      event.preventDefault();
+      showThankMessage();
+    }
+
+    function addListeners() {
+      window.addEventListener('scroll', showPopup);
+      yesButton.addEventListener('click', showNewsletter);
+      noButton.addEventListener('click', showSorryMessage);
+      cross.addEventListener('click', hideNewsletter);
+      submit.addEventListener('click', onSubmitButtonClick);
+    }
+
+    function show() {
+      addVisibleClass(popupElement);
       addVisibleClass(question);
-      localStorage.popupClosed = true;
     }
-  }
 
-  function showNewsletter() {
-    removeVisibleClass(question);
-    addVisibleClass(newsletter);
-  }
-
-  function showSorryMessage() {
-    removeVisibleClass(question);
-    addVisibleClass(sorryMessage);
-    setTimeout(removeVisibleClass, 4000, sorryMessage);
-    setTimeout(removeVisibleClass, 4000, popup);
-  }
-
-  function hideNewsletter() {
-    removeVisibleClass(newsletter);
-    removeVisibleClass(popup);
-  }
-
-  function showThankMessage() {
-    removeVisibleClass(newsletter);
-    addVisibleClass(thankMessage);
-    setTimeout(removeVisibleClass, 3000, thankMessage);
-    setTimeout(removeVisibleClass, 3000, popup);
-  }
-
-  function submitButton(evt) {
-    evt.preventDefault();
-    showThankMessage();
-  }
-
-  function addListeners () {
-    window.addEventListener('scroll', showPopup);
-    yesButton.addEventListener('click', showNewsletter);
-    noButton.addEventListener('click', showSorryMessage);
-    cross.addEventListener('click', hideNewsletter);
-    submit.addEventListener('click', submitButton);
-  }
-
-  function show() {
-    addVisibleClass(popup);
-    addVisibleClass(question);
-  }
-
-  function hide() {
-    removeVisibleClass(question);
-    removeVisibleClass(newsletter);
-    removeVisibleClass(thankMessage);
-    removeVisibleClass(sorryMessage);
-    removeVisibleClass(popup);
-  }
-
-  function init() {
-    popup = document.querySelector('.popup');
-    question = popup.querySelector('.question');
-    newsletter = popup.querySelector('.newsletter');
-    sorryMessage = popup.querySelector('.sorry-message');
-    thankMessage = popup.querySelector('.thank-message');
-    pageHeight = Math.max(
-      document.body.scrollHeight, document.documentElement.scrollHeight,
-      document.body.offsetHeight, document.documentElement.offsetHeight,
-      document.body.clientHeight, document.documentElement.clientHeight
-    );
-    scrollHeight = pageHeight - document.documentElement.clientHeight;
-    yesButton = popup.querySelector('.yes-button');
-    noButton = popup.querySelector('.no-button');
-    cross = popup.querySelector('.cross');
-    submit = popup.querySelector('.submit-button');
-
-    if (!localStorage.popupClosed) {
-      addListeners();
+    function hide() {
+      removeVisibleClass(question);
+      removeVisibleClass(newsletter);
+      removeVisibleClass(thankMessage);
+      removeVisibleClass(sorryMessage);
+      removeVisibleClass(popupElement);
     }
-    else {console.log('Не работает')}
-  }
-  init();
 
-  return  {
-    init,
-    show,
-    hide
-  }
-  }
-  )();
+    function init() {
+      popupElement = document.querySelector('.popup');
+      question = popupElement.querySelector('.question');
+      newsletter = popupElement.querySelector('.newsletter');
+      sorryMessage = popupElement.querySelector('.sorry-message');
+      thankMessage = popupElement.querySelector('.thank-message');
+      yesButton = popupElement.querySelector('.yes-button');
+      noButton = popupElement.querySelector('.no-button');
+      cross = popupElement.querySelector('.cross');
+      submit = popupElement.querySelector('.submit-button');
 
+      if (!localStorage.popupClosed) {
+        addListeners();
+      }
+    }
+
+    return {
+      init,
+      show,
+      hide
+    }
+  })();
+
+window.articlePopup.init();
