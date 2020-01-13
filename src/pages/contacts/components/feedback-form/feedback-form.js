1e1f18737024693353068d1fa0392dbf7d5a6e32
Ivan Gromov <50546979+Gr0mi4@users.noreply.github.com>
2020-01-13 21:07:12 +0300

    another refactoring was made, module system used, bug with sending a message with empty fields, fixed.

--- src/pages/contacts/components/feedback-form/feedback-form.js
+++ src/pages/contacts/components/feedback-form/feedback-form.js
@@ -4,56 +4,73 @@ window.feedbackForm = (function () {
   const emailInput = document.getElementById('email-input');
   const messageInput = document.getElementById('message-input');
   const submitButton = document.getElementById('submit-button');
-  const nameInputContainer = document.getElementById('name-input-container');
-  const emailInputContainer = document.getElementById('email-input-container');
-  const messageInputContainer = document.getElementById('message-input-container');
 
-  let sendingBlocked = false;
+  let sendingBlocked;
 
-  nameInput.onblur = () => {
-    const nameIsValid = nameInput.value.match(/^.+\D\S$/i);
-    if (nameIsValid) {
-      nameInputContainer.classList.remove('invalid');
-      nameInput.classList.remove('invalid-input');
+
+  function init() {
+    nameInput.onblur = () => {
+      const nameIsValid = nameInput.value.match(/^.+\D\S$/i);
+      if (nameIsValid) {
+        showInvalidInputError(nameInput, 'hide');
+      } else {
+        showInvalidInputError(nameInput)
+      }
+    };
+
+    emailInput.onblur = () => {
+      const emailIsValid = emailInput.value.match(/^.+@.+\.\w+$/i);
+      if (emailIsValid) {
+        showInvalidInputError(emailInput, 'hide');
+      } else {
+        showInvalidInputError(emailInput)
+      }
+    };
+
+    messageInput.onblur = () => {
+      const containsBadWords = messageInput.value.match(/Хуй|Пизд|Еб[а@]н|Уе[б6]|Бля[дт]|Пид[оoаa0]р/i);
+      if (containsBadWords) {
+        showInvalidInputError(messageInput);
+      } else {
+        showInvalidInputError(messageInput, 'hide');
+      }
+    };
+
+    submitButton.onclick = (event) => {
+      if (sendingBlocked) {
+        event.preventDefault();
+      }else if(nameInput.value === "") {
+        event.preventDefault();
+        showInvalidInputError(nameInput)
+      } else if(emailInput.value === "") {
+        event.preventDefault();
+        showInvalidInputError(emailInput)
+      } else {
+        showInvalidInputError(nameInput, 'hide');
+        showInvalidInputError(emailInput, 'hide');
+        alert('Message sent!');
+      }
+    };
+  }
+
+  function showInvalidInputError(element, action = 'show') {
+    if (action === 'show') {
+      element.parentElement.classList.add('invalid');
+      element.classList.add('invalid-input');
       sendingBlocked = true;
-    } else {
-      nameInputContainer.classList.add('invalid');
-      nameInput.classList.add('invalid-input');
+    }
+    else if (action === 'hide') {
+      element.parentElement.classList.remove('invalid');
+      element.classList.remove('invalid-input');
       sendingBlocked = false;
     }
-  };
+  }
 
-  emailInput.onblur = () => {
-    const emailIsValid = emailInput.value.match(/^.+@.+\.\w+$/i);
-    if (emailIsValid) {
-      emailInputContainer.classList.remove('invalid');
-      emailInput.classList.remove('invalid-input');
-      sendingBlocked = false;
-    } else {
-      emailInputContainer.classList.add('invalid');
-      emailInput.classList.add('invalid-input');
-      sendingBlocked = true;
-    }
-  };
-
-  messageInput.onblur = () => {
-    const containsBadWords = messageInput.value.match(/Хуй|Пизд|Еб[а@]н|Уе[б6]|Бля[дт]|Пид[оoаa0]р/i);
-    if (containsBadWords) {
-      messageInputContainer.classList.add('invalid-message');
-      messageInput.classList.add('invalid-input');
-      sendingBlocked = true;
-    } else {
-      messageInputContainer.classList.remove('invalid-message');
-      messageInput.classList.remove('invalid-input');
-      sendingBlocked = false;
-    }
-  };
-
-  submitButton.onclick = (event) => {
-    if (sendingBlocked) {
-      event.preventDefault();
-    } else {
-      alert('Message sent!');
-    }
-  };
+  return {
+    init,
+    showInvalidInputError,
+    nameInput,
+    emailInput,
+    messageInput
+  }
 })();
